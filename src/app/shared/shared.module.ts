967244894b02b97c6b5e6c6f56e2eee57551d438
Enrique Rios <riosoft@outlook.com>
2023-06-30 17:26:09 -0600

    Primer RouterLink

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
-
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomePageComponent,
+    AboutPageComponent,
+    ContactPageComponent,
+    SidebarComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
+  ],
+  exports: [
+    HomePageComponent,
+    AboutPageComponent,
+    ContactPageComponent,
+    SidebarComponent
   ]
 })
 export class SharedModule { }
